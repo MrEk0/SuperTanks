d17049d5a990b7e5208c9c464bb1d5b915b08f6e
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-01-02 21:53:52 +0300

    player movement

---
+++ SuperTanks/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float speed = 5f;
+
+    Rigidbody2D rb;
+
+    float speedX;
+    float speedY;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalPos = Input.GetAxisRaw("Vertical");
+        speedY = verticalPos * speed;
+
+        float horizontalPos = Input.GetAxisRaw("Horizontal");
+        speedX = horizontalPos * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
