d6a9b2a5fe818fe26190bd0962d50510564ca0bd
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-01-04 20:54:30 +0300

    fire

---
+++ SuperTanks/Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed=10f;
+    [SerializeField] GameObject explosionPrefab;
+    [SerializeField] float timeToDestroyExplosion = 0.35f;
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.AddForce(transform.up * speed, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            return;
+
+        //set up explosion
+        GameObject explosion=Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(explosion, timeToDestroyExplosion);
+
+        //if (collision.CompareTag("Enemy"))
+        //{
+        //    //damage
+        //}
+
+        Destroy(gameObject);
+    }
+}
---
+++ SuperTanks/Assets/Scripts/Fire.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float fireSpeed = 1f;
+
+    float timeSinceLastShot = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //fire
+            if(timeSinceLastShot>fireSpeed)
+            {
+                Instantiate(bulletPrefab, transform.position, transform.rotation);
+                timeSinceLastShot = 0f;
+            }           
+        }
+
+        timeSinceLastShot += Time.deltaTime;
+    }
+}
