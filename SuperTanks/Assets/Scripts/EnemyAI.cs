6e3f683756b4c522a4913cbccbc8011b8f21fbeb
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-01-06 18:15:15 +0300

    rotate enemy

--- SuperTanks/Assets/Scripts/EnemyAI.cs
+++ SuperTanks/Assets/Scripts/EnemyAI.cs
@@ -1,6 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using UnityEngine;
+
+public enum Directions
+{
+    Right,
+    Left,
+    Up,
+    Down
+}
 
 public class EnemyAI : MonoBehaviour
 {
@@ -10,18 +20,17 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] float rayDistance = 10f;
     [SerializeField] float offset=0.5f;
 
-    Rigidbody2D rb;
+    //Rigidbody2D rb;
     Vector2 targetPos;
     Vector2 previousTarget;
     Vector2 currentPoint;
     LayerMask mask;
     Vector2[] directions = new Vector2[4] { Vector2.right, Vector2.left, Vector2.up, Vector2.down };
     Vector3[] offsets;
+    //Vector2 rotateDirection;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        mask = LayerMask.GetMask("WayPoint");
         offsets = new Vector3[4] { new Vector3(offset, 0f), new Vector3(-offset, 0f), new Vector3(0f, offset), new Vector3(0f, -offset) };
     }
 
@@ -35,55 +44,54 @@ public class EnemyAI : MonoBehaviour
     {
         if (!isReachedTargetPoint())
         {
-            transform.position=Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
         else
         {
             do
             {
                 targetPos = GetTargetPos();
-                Debug.Log(targetPos);
             }
             while (targetPos == previousTarget);
 
-            if(currentPoint!=null)
+            if (currentPoint != null)
             {
                 previousTarget = currentPoint;
             }
 
             currentPoint = targetPos;
-
-            
         }
     }
 
     private Vector2 GetTargetPos()
     {
         Vector2 target;
-        List<Vector2> targetPositions = new List<Vector2>();
+        Vector2 rotateDirection;
+        Dictionary<Vector2, Vector2> targetDirections = new Dictionary<Vector2, Vector2>();
 
         for (int i = 0; i < directions.Length; i++)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position+offsets[i], directions[i] /*, rayDistance, mask*/);
-            //Debug.Log(hit.collider.gameObject.layer==LayerMask.NameToLayer("WayPoint"));
-            //Debug.Log(hit.collider.IsTouchingLayers(LayerMask.NameToLayer("WayPoint")));
-            if(hit.collider.gameObject.layer == LayerMask.NameToLayer("WayPoint"))
+            RaycastHit2D hit = Physics2D.Raycast(transform.position + offsets[i], directions[i]);
+
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("WayPoint"))
             {
-                targetPositions.Add(hit.transform.position);
-                //Debug.Log(hit.collider.gameObject);
+                targetDirections.Add(hit.transform.position, directions[i]);
             }
         }
 
-        if(targetPositions.Count!=0)
+        if(targetDirections.Count !=0)
         {
-            int targetPoint=Random.Range(0, targetPositions.Count);
-            target = targetPositions[targetPoint];
+            int targetPoint = UnityEngine.Random.Range(0, targetDirections.Count);
+            target = targetDirections.ElementAt(targetPoint).Key;
+            rotateDirection= targetDirections.ElementAt(targetPoint).Value;
         }
         else
         {
             target = transform.position;
+            rotateDirection = Vector2.zero;
         }
 
+        RotateTank(rotateDirection);
 
         return target;
     }
@@ -96,5 +104,32 @@ public class EnemyAI : MonoBehaviour
     }
 
 
+    private void RotateTank(Vector2 direction)
+    {
+        if (direction == Vector2.zero)
+        {
+            return;
+        }
 
+        float angle = 0f;
+        Directions dir =(Directions) Array.IndexOf(directions, direction);
+
+        switch(dir)
+        {
+            case Directions.Right:
+                angle = -90f;
+                break;
+            case Directions.Left:
+                angle = 90f;
+                break;
+            case Directions.Up:
+                angle = 0f;
+                break;
+            case Directions.Down:
+                angle = 180f;
+                break;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 }
