f6b670aa11e157c081187391ad510054cf8c5573
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-01-16 16:50:38 +0300

    fuel pick up

--- SuperTanks/Assets/Scripts/Movement.cs
+++ SuperTanks/Assets/Scripts/Movement.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] float speedSensity = 0.15f;
-    [SerializeField] GameObject tracePrefab;
-    [SerializeField] Transform traceStartPos;
-    [SerializeField] float traceLivingTime = 0.5f;
-    [SerializeField] float traceDropFrequency = 1f;
+    [SerializeField] float amountOfFuel = 10f;
+    [SerializeField] Slider fuelSlider;
+    //[SerializeField] GameObject tracePrefab;
+    //[SerializeField] Transform traceStartPos;
+    //[SerializeField] float traceLivingTime = 0.5f;
+    //[SerializeField] float traceDropFrequency = 1f;
 
     Rigidbody2D rb;
 
@@ -19,21 +22,64 @@ public class Movement : MonoBehaviour
     Transform myTransform;
     float angle;
     bool isMoving = false;
-    float timeSinceLastTraceDrop = Mathf.Infinity;
+    //float timeSinceLastTraceDrop = Mathf.Infinity;
     float pressedVerTime = 0f;
     float pressedHorTime = 0f;
+    float currentFuelAmount;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         myTransform = GetComponent<Transform>();
+
+        currentFuelAmount = amountOfFuel;
     }
     void Update()
     {
         float verticalPos = Input.GetAxisRaw("Vertical");
         float horizontalPos = Input.GetAxisRaw("Horizontal");
 
+        if (verticalPos != 0 || horizontalPos != 0)
+        {
+            VerticalMovement(verticalPos, horizontalPos);
+            HorizontalMovement(verticalPos, horizontalPos);
+            FuelConsumption();
+        }
+        else
+        {
+            speedX = 0f;
+            speedY = 0f;
+        }
+        //LeaveTrace();
+    }
+
+    private void HorizontalMovement(float verticalPos, float horizontalPos)
+    {
+        if (horizontalPos != 0 && verticalPos == 0)
+        {
+            pressedHorTime += Time.deltaTime;
+
+            if (speedSensity < pressedHorTime)
+            {
+                speedX = horizontalPos * speed;
+                speedY = 0f;
+
+            }
+
+            angle = horizontalPos > 0 ? -90f : 90f;
+            myTransform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else
+        {
+            pressedHorTime = 0f;
+            speedX = 0f;
+            myTransform.position = new Vector3(Mathf.RoundToInt(myTransform.position.x), myTransform.position.y);
+        }
+    }
+
+    private void VerticalMovement(float verticalPos, float horizontalPos)
+    {
         if (verticalPos != 0 && horizontalPos == 0)
         {
             pressedVerTime += Time.deltaTime;
@@ -52,29 +98,6 @@ public class Movement : MonoBehaviour
             speedY = 0f;
             myTransform.position = new Vector3(myTransform.position.x, Mathf.RoundToInt(myTransform.position.y));
         }
-
-        if (horizontalPos != 0 && verticalPos == 0)
-        {
-            pressedHorTime += Time.deltaTime;
-
-            if (speedSensity < pressedHorTime)
-            {
-                speedX = horizontalPos * speed;
-                speedY = 0f;
-                
-            }
-
-            angle = horizontalPos > 0 ? -90f : 90f;
-            myTransform.rotation = Quaternion.Euler(0, 0, angle);
-        }
-        else
-        {
-            pressedHorTime = 0f;
-            speedX = 0f;
-            myTransform.position = new Vector3(Mathf.RoundToInt(myTransform.position.x), myTransform.position.y);
-        }
-
-        LeaveTrace();
     }
 
     private void FixedUpdate()
@@ -82,29 +105,48 @@ public class Movement : MonoBehaviour
         rb.velocity = new Vector2(speedX, speedY);
     }
 
-    private void LeaveTrace()
+    private void FuelConsumption()
     {
-        if(speedX==0 && speedY==0)
+        if (Mathf.Max(currentFuelAmount, 0f) != 0f) 
         {
-            isMoving = false;
-            timeSinceLastTraceDrop = Mathf.Infinity;
+            currentFuelAmount -= Time.deltaTime;
+            fuelSlider.value = currentFuelAmount / amountOfFuel;
         }
         else
         {
-            isMoving = true;
-        }
-
-        if (isMoving )
-        {
-            if (timeSinceLastTraceDrop > traceDropFrequency) 
-            {
-                GameObject trace = Instantiate(tracePrefab, myTransform.position, Quaternion.Euler(0, 0, angle));
-                Destroy(trace, traceLivingTime);
-                timeSinceLastTraceDrop = 0f;
-            }
-
-            timeSinceLastTraceDrop += Time.deltaTime;
-            
+            Debug.Log("Run out of Fuel");
         }
     }
+
+    public void Refill(float fuelAmount)
+    {
+        currentFuelAmount = Mathf.Min(amountOfFuel, currentFuelAmount+fuelAmount);
+        fuelSlider.value = currentFuelAmount / amountOfFuel;
+    }
+
+    //private void LeaveTrace()
+    //{
+    //    if(speedX==0 && speedY==0)
+    //    {
+    //        isMoving = false;
+    //        timeSinceLastTraceDrop = Mathf.Infinity;
+    //    }
+    //    else
+    //    {
+    //        isMoving = true;
+    //    }
+
+    //    if (isMoving )
+    //    {
+    //        if (timeSinceLastTraceDrop > traceDropFrequency) 
+    //        {
+    //            GameObject trace = Instantiate(tracePrefab, myTransform.position, Quaternion.Euler(0, 0, angle));
+    //            Destroy(trace, traceLivingTime);
+    //            timeSinceLastTraceDrop = 0f;
+    //        }
+
+    //        timeSinceLastTraceDrop += Time.deltaTime;
+            
+    //    }
+    //}
 }
