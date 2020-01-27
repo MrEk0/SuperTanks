44472e102a470fe7dd3275396014c746e822a76e
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-01-27 16:44:09 +0300

    level Loader

---
+++ SuperTanks/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] float timeToLoad = 2f;
+    //[SerializeField] Animator animator;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        animator.SetTrigger("LoadLevel");
+        yield return new WaitForSeconds(timeToLoad);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
