4dafb039a45c9d4b8fdb0326f2f327a0342c1eaf
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-02-25 22:31:44 +0300

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public bool IsGamePause { get; private set; } = false;
+
+    private void Awake()
+    {
+       if(instance!=null && instance!=this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PauseGame()
+    {
+        AudioManager.PlayUIButtonAudio();
+        IsGamePause = true;
+        AudioManager.StopAllTankSounds();
+    }
+
+    public void ResumeGame()
+    {
+        AudioManager.PlayUIButtonAudio();
+        IsGamePause = false;
+    }
+}
