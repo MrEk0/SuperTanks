78b85a20fcbf49120b14ddb37235533164f1f14b
MrEk0 <51297787+MrEk0@users.noreply.github.com>
2020-02-26 18:26:29 +0300

    hierarchy polish
    create extra folders

---
+++ Assets/Scripts/UI/SettingsSlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsSlider : MonoBehaviour
+{
+    [SerializeField] Image volumeOnImage;
+    [SerializeField] Sprite volumeOffSprite;
+
+    Sprite startSprite;
+
+    float minimumVolume;
+
+    private void Awake()
+    {
+        startSprite = volumeOnImage.sprite;
+
+        minimumVolume = GetComponent<Slider>().minValue;
+    }
+
+    public void SetImage(float volume)
+    {
+        if (volume <= minimumVolume)
+        {
+            volumeOnImage.sprite = volumeOffSprite;
+        }
+        else
+        {
+            volumeOnImage.sprite = startSprite;
+        }
+    }
+}
